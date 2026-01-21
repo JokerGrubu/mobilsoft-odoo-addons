2433e1a7ba45e6465fd3c0691593a115c0f30642
MobilSoft <info@mobilsoft.net>
2026-01-21 21:45:01 +0100

    Auto-sync: 2026-01-21 21:45

--- mobilsoft_website_fix/controllers/main.py
+++ mobilsoft_website_fix/controllers/main.py
@@ -9,8 +9,10 @@ class Website(website_main.Website):
     
     def _login_redirect(self, uid, redirect=None):
         """ Login sonrası /web route'una yönlendir (database session'da zaten var) """
-        if not redirect and request.params.get('login_success'):
-            if request.env['res.users'].browse(uid)._is_internal():
+        # Internal kullanıcılar için /web'e yönlendir
+        if not redirect:
+            user = request.env['res.users'].browse(uid)
+            if user._is_internal():
                 # Database session'da zaten var, sadece /web'e yönlendir
                 redirect = '/web'
             else:
