803bd33bdeb1dcbdf583207e316fed85b8370081
MobilSoft <info@mobilsoft.net>
2026-01-21 13:45:01 +0100

    Auto-sync: 2026-01-21 13:45

--- mobilsoft_account_patch/__init__.py
+++ mobilsoft_account_patch/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2024 Odoo Turkey Community (https://github.com/orgs/Odoo-Turkey-Community/dashboard)
 # License Other proprietary. Please see the license file in the Addon folder.
-# Updated for Odoo 19 compatibility - 21 Ocak 2026
 
 from odoo.addons.account.models.chart_template import AccountChartTemplate
 
@@ -10,20 +9,16 @@ origin_try_loading = AccountChartTemplate.try_loading
 AccountChartTemplate.origin_try_loading = origin_try_loading
 
 
-def new_try_loading(self, template_code, company, install_demo=False, force_create=True):
-    """
-    Odoo 19 uyumlu try_loading override.
-    Türkiye muhasebe şablonlarının otomatik yüklenmesini engeller,
-    kullanıcının kendi seçmesine izin verir.
-    """
-    # Türkiye muhasebe şablonları
-    tr_templates = ['tr', 'l10n_tr', 'tr_7a', 'tr_7b', 'l10n_tr_7a', 'l10n_tr_7b']
-    
-    # İlk kurulumda force gelmemiş ise Türkiyenin muhasebe hesap ve vergilerini yükleme
-    # kullanıcı kendi seçsin
-    if not self.env.context.get("force", False) and template_code in tr_templates:
+def new_try_loading(self, company=False, install_demo=True):
+    l10n_tr_chart = [
+        self.env.ref("l10n_tr.chart_template_common", False),
+        self.env.ref("l10n_tr.chart_template_7a", False),
+        self.env.ref("l10n_tr.chart_template_7b", False),
+    ]
+    # İlk kurulumda force gelmemiş ise Türkiyenin muhasebe hesap ve vergilerini yükleme kullanıcı kendi seçsin
+    if not self.env.context.get("force", False) and self in l10n_tr_chart:
         return
-    return self.origin_try_loading(template_code, company, install_demo, force_create)
+    return self.origin_try_loading(company, install_demo)
 
 
 AccountChartTemplate.try_loading = new_try_loading
