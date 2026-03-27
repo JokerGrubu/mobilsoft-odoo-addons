9f29849ee8b347329688f3eef15d847ed9d81050
MobilSoft <info@mobilsoft.net>
2026-03-27 19:50:01 +0300

    Auto-sync: 2026-03-27 19:50

--- dbfilter_from_header/__init__.py
+++ dbfilter_from_header/__init__.py
@@ -1,17 +1,17 @@
 import logging
 from odoo import http
-from odoo.tools import config
 
 _logger = logging.getLogger(__name__)
 
 # Orijinal odoo.http.db_filter fonksiyonunu sakla
 orig_db_filter = http.db_filter
 
-def dbfilter_from_header(dbs, httprequest=None):
-    if httprequest:
-        custom_db = httprequest.headers.get('X-Odoo-dbfilter')
+def dbfilter_from_header(dbs, host=None, **kwargs):
+    request = http.request
+    if request and hasattr(request, 'httprequest'):
+        custom_db = request.httprequest.headers.get('X-Odoo-dbfilter')
         if custom_db and custom_db in dbs:
             return [custom_db]
-    return orig_db_filter(dbs, httprequest)
+    return orig_db_filter(dbs, host=host, **kwargs)
 
 http.db_filter = dbfilter_from_header
